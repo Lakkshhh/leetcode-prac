3a5f76273e7ceb3cec5ec4a7cb94b44f81c66443
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2024-05-03 13:02:22 +0530

    Time: 17 ms (21.18%), Space: 44.6 MB (33.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    if (s.length()!= t.length()) {
+        return false;
+    }
+
+    HashMap<Character, Integer> hc = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        hc.put(c, hc.getOrDefault(c, 0) + 1);
+    }
+    for (int j = 0; j < t.length(); j++) {
+        char c = t.charAt(j);
+        if (hc.containsKey(c)) {
+            int count = hc.get(c) - 1;
+            if (count == 0) {
+                hc.remove(c);
+            } else {
+                hc.put(c, count);
+            }
+        } else {
+            return false;
+        }
+    }
+    return hc.isEmpty();
+}
+}
