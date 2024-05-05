85ed5d29829d186faf896589242633b2f4b3c5a0
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2024-05-05 22:10:32 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i<nums.length-1; i++) {
-            for(int j = i+1; j<nums.length; j++) {
-                if(nums[i]+nums[j]==target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            int compliment = target - nums[i];
+            if(count.containsKey(compliment)) {
+                return new int[] {count.get(compliment), i};
             }
+            count.put(nums[i], i);
         }
-        return new int[0];
+        return new int[]{};
     }
 }
