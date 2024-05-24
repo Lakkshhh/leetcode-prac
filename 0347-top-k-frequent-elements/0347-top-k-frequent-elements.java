1ad18ce543ade67d5e929267a4b1e7c31d897573
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2024-05-25 00:29:17 +0530

    Update 0347-top-k-frequent-elements.java

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Calculate the frequency of each element
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int freq : nums) {
+            map.put(freq, map.getOrDefault(freq, 0) + 1);
         }
-
-        // Use a min-heap to keep track of the top K frequent elements
-        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
-            (a, b) -> a.getValue() - b.getValue()
-        );
-
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            heap.offer(entry);
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
-
-        // Extract the elements from the heap into the result array
-        int[] elements = new int[k];
+        
+        List<Integer> ls = new ArrayList<>(map.keySet());
+        Collections.sort(ls, (a, b) -> map.get(b) - map.get(a));
+        int[] result = new int[k];
         int index = 0;
-        while (!heap.isEmpty()) {
-            elements[index++] = heap.poll().getKey();
+        
+        for(int i = 0; i<k; i++) {
+            result[index] = ls.get(i);
+            index++;
         }
-
-        return elements;
+        return result;
     }
 }
