3a944790c704659d6c20d718d72e4d57b7c61c9b
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2024-06-19 14:48:41 +0530

    Time: 2 ms (52.72%), Space: 43 MB (54.16%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> output_arr = new ArrayList<String>();
-        backtrack(output_arr, "", 0, 0, n);
-        return output_arr;
+        List<String> res = new ArrayList<String>();
+        backtrack(res, "", 0, 0, n);
+        return res;
     }
     
-    public void backtrack(List<String> output_arr, String s, int open, int close, int n) {
+    public void backtrack(List<String> res, String s, int open, int close, int n) {
         if (s.length() == n * 2) {
-            output_arr.add(s);
+            res.add(s);
             return;
         }
         
         if (open < n) {
-            backtrack(output_arr, s + "(", open + 1, close, n);
+            backtrack(res, s + "(", open + 1, close, n);
         }
         
         if (close < open) {
-            backtrack(output_arr, s + ")", open, close + 1, n);
+            backtrack(res, s + ")", open, close + 1, n);
         }
     }
 	// See above tree diagram with parameters (left, right, s) for better understanding
