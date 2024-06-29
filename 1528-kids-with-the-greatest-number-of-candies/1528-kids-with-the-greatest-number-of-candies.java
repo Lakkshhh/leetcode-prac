ffdba1d75ac0e5d2847d6ed3bdba00107c731498
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2024-06-30 01:07:55 +0530

    Time: 1 ms (89.12%), Space: 42.3 MB (44.4%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res = new LinkedList<>();
+        int max = 0;
+        for(int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        for(int i = 0; i < candies.length; i++) {
+            if(candies[i] + extraCandies >= max) {
+                res.add(true);
+            } else {
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
