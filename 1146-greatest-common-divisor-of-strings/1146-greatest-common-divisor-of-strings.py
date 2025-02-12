95001282c4d50b65770570dce359bd031fc73ef5
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2025-02-12 13:13:47 -0500

    Time: 0 ms (100%), Space: 17.7 MB (67.69%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,11 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if (str1+str2) == (str2+str1):
-            maxString = min(len(str1), len(str2))
-            result = 0
-            for i in range(1, maxString+1):
-                if len(str1)%i == 0 and len(str2)%i == 0:
-                    result = i
-            return str(str1[:result])
+            maxString = gcd(len(str1), len(str2))
+            return str(str1[:maxString])
         else:
             return ""
