f2e25cc6ffffed37b3d214cebfddee879a00d066
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2025-03-18 16:10:50 -0400

    Time: 25 ms (53.35%), Space: 23.1 MB (93.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,5 @@
 class Solution:
-    # def prod(self, n, nums):
+    # def prod(self, n, nums): #brute force
     #     product = 1
     #     for i in range(len(nums)):
     #         if i != n: 
@@ -13,18 +13,32 @@ class Solution:
         #     ans.append(mul)
 
         # return ans
-        n=len(nums)
-        ans, pre, suf = [0] * n, [0] * n, [0] * n
-        pre[0] = 1
-        suf[n-1] = 1
 
-        for i in range(1, n):
-            pre[i] = pre[i-1]*nums[i-1]
+        # n=len(nums) # This uses prefix/postfix but space complexity not great
+        # ans, pre, suf = [0] * n, [0] * n, [0] * n
+        # pre[0] = 1
+        # suf[n-1] = 1
 
-        for i in range(n-2, -1, -1):
-            suf[i] = suf[i+1]*nums[i+1]
+        # for i in range(1, n):
+        #     pre[i] = pre[i-1]*nums[i-1]
 
-        for i in range(n):
-            ans[i] = pre[i]*suf[i]
+        # for i in range(n-2, -1, -1):
+        #     suf[i] = suf[i+1]*nums[i+1]
+
+        # for i in range(n):
+        #     ans[i] = pre[i]*suf[i]
+
+        # return ans
+        n = len(nums)
+        ans = [0]*n
+        prefix, postfix = 1, 1
+
+        for i in range(len(nums)):
+            ans[i] = prefix
+            prefix *= nums[i]
+
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
 
         return ans
