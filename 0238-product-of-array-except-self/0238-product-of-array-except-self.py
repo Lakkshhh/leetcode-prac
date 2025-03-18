56530888b184ce51be7f86d0c102bfba8e89312e
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2025-03-18 16:13:23 -0400

    Time: 19 ms (87.1%), Space: 23.2 MB (79.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -29,13 +29,15 @@ class Solution:
         #     ans[i] = pre[i]*suf[i]
 
         # return ans
-        ans = [0]*len(nums)
-        prefix, postfix = 1, 1
+        
+        ans = [1]*len(nums)
 
+        prefix = 1
         for i in range(len(nums)):
             ans[i] = prefix
             prefix *= nums[i]
 
+        postfix = 1
         for i in range(len(nums)-1, -1, -1):
             ans[i] *= postfix
             postfix *= nums[i]
