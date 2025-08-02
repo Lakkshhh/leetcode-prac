768448ec77c4ec5d2739bd57e07ab085e748b9bf
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2025-08-02 20:08:22 +0530

    Time: 4 ms (57.2%), Space: 45.1 MB (24.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,50 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
+        ArrayList<Character> cleanString = new ArrayList<>();
+
+        for(char i: s.toCharArray()) {
+            if(Character.isLetterOrDigit(i)) {
+                i = Character.toLowerCase(i);
+                cleanString.add(i);
+            }
         }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+
+        int left = 0;
+        int right = cleanString.size() - 1;
+
+        while(left < right) {
+            if(cleanString.get(left).equals(cleanString.get(right))) {
+                left++;
+                right--;
+            }
+            else {
+                return false;
+            }
         }
         return true;
     }
 }
-
-
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         String fixed_string = "";
-//         for(char c : s.toCharArray()) {
-//             if(Character.isDigit(c) || Character.isLetter(c)) {
-//                 fixed_string += c;
-//             }
-//         }
-//         fixed_string = fixed_string.toLowerCase();
-        
-//         int a_pointer = 0;
-//         int b_pointer = fixed_string.length() - 1;
-        
-//         while(a_pointer <= b_pointer) {
-//             if(fixed_string.charAt(a_pointer) != fixed_string.charAt(b_pointer)) {
-//                 return false;
-//             }
-//             a_pointer += 1;
-//             b_pointer -= 1;
-//         }
-//         return true;
-//     }
-// }
