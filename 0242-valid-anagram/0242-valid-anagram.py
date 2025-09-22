04e8ae9fbc0e961f13cb1f606043c3aea2f1e747
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2025-09-22 15:17:34 -0400

    Time: 20 ms (6.32%), Space: 18.6 MB (5.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # count1 = {}
-        # count2 = {}
+        if len(s) != len(t):
+            return False
 
-        # for char in s:
-        #     count1.update({char : count1.get(char, 0) + 1})
-
-        # for char in t:
-        #     count2.update({char : count2.get(char, 0) + 1})
-        
-        # return count1 == count2
-        return sorted(list(t)) == sorted(list(s))
+        return sorted(s) == sorted(t)
