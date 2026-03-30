1e80231300ad00e38270f96ce352311c3489ca95
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-03-30 17:43:31 -0400

    Time: 0 ms (100%), Space: 19.3 MB (51.34%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -16,3 +16,40 @@ class Solution:
             return memo[i]
         
         return dfs(0)
+
+        # Bottom Up Dynamic Programming Approach
+
+        # if len(nums) == 1:
+        #     return nums[0]
+        # return max(self.helper(nums[1:]),
+        #            self.helper(nums[:-1]))
+
+        # def helper(self, nums: List[int]) -> int:
+        #     if not nums:
+        #         return 0
+        #     if len(nums) == 1:
+        #         return nums[0]
+
+        #     dp = [0] * len(nums)
+        #     dp[0] = nums[0]
+        #     dp[1] = max(nums[0], nums[1])
+
+        #     for i in range(2, len(nums)):
+        #         dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+        #     return dp[-1]
+
+
+        # Space Optimized Approach
+
+        # return max(nums[0], self.helper(nums[1:]),
+        #                     self.helper(nums[:-1]))
+
+        # def helper(self, nums):
+        #     rob1, rob2 = 0, 0
+
+        #     for num in nums:
+        #         newRob = max(rob1 + num, rob2)
+        #         rob1 = rob2
+        #         rob2 = newRob
+        #     return rob2
