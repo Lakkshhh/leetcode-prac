1ab8b3c395ad7e63faa2927ca741c37899027049
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-04-04 18:02:06 -0400

    Time: 11 ms (23.3%), Space: 24.8 MB (21.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        nums.sort()
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-        
-        heap = []
-        for num, count in freq.items():
-            heapq.heappush(heap, (count, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
+        # Bucket Sort Approach
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
-        ans = []
-        for i in range(k):
-            ans.append(heapq.heappop(heap)[1])
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        for num, cnt in count.items():
+            freq[cnt].append(num)
         
-        return ans
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+        # Min-Heap Approach
+        # freq = {}
+        # nums.sort()
+        # for num in nums:
+        #     freq[num] = freq.get(num, 0) + 1
+        
+        # heap = []
+        # for num, count in freq.items():
+        #     heapq.heappush(heap, (count, num))
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+
+        # ans = []
+        # for i in range(k):
+        #     ans.append(heapq.heappop(heap)[1])
+        
+        # return ans
