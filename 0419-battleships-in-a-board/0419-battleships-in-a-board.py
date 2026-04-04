e1ce3e6386fc7c7558627841daf474f728c3c1bd
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-04-04 18:14:25 -0400

    Time: 3 ms (55.98%), Space: 21.1 MB (84.13%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,28 +1,42 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        rows, cols = len(board), len(board[0])
-        visited = []
-        islands = 0
-        if not board:
-            return 0
+        # Idea is that each ship's starting point has "." above it and left of it, so it is easy to track a whole ship just using that when traversing through entire grid
 
-        def bfs(i, j):
-            queue = collections.deque()
-            visited.append((i, j))
-            queue.append((i, j))
-            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        count = 0
 
-            while queue:
-                row, col = queue.popleft()
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if r in range(rows) and c in range(cols) and (r, c) not in visited and board[r][c] == "X":
-                        visited.append((r, c))
-                        queue.append((r, c))
+        for i, row in enumerate(board):
+            for j, cell in enumerate(row):
+                if cell == "X":
+                    if (i == 0 or board[i - 1][j] == ".") and\
+                       (j == 0 or board[i][j - 1] == "."):
+                            count += 1
+                            
+        return count
 
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == "X" and (i, j) not in visited:
-                    bfs(i, j)
-                    islands += 1
-        return islands
+        # BFS Approach, can be done in DFS as well using recursion
+        # rows, cols = len(board), len(board[0])
+        # visited = set()
+        # islands = 0
+        # if not board:
+        #     return 0
+
+        # def bfs(i, j):
+        #     queue = collections.deque()
+        #     visited.add((i, j))
+        #     queue.append((i, j))
+        #     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        #     while queue:
+        #         row, col = queue.popleft()
+        #         for dr, dc in directions:
+        #             r, c = row + dr, col + dc
+        #             if r in range(rows) and c in range(cols) and (r, c) not in visited and board[r][c] == "X":
+        #                 visited.add((r, c))
+        #                 queue.append((r, c))
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if board[i][j] == "X" and (i, j) not in visited:
+        #             bfs(i, j)
+        #             islands += 1
+        # return islands
