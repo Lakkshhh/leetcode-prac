faea596d9e3251bca2fc413c8ad3a5f2a761ab47
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-04-05 14:56:10 -0400

    Time: 14 ms (95.57%), Space: 25.5 MB (74.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,45 +1,15 @@
 class Solution:
-    # def prod(self, n, nums): #brute force
-    #     product = 1
-    #     for i in range(len(nums)):
-    #         if i != n: 
-    #             product = product * nums[i]
-    #     return product
-
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # ans=[]
-        # for i in range(len(nums)):
-        #     mul = self.prod(i, nums)
-        #     ans.append(mul)
-
-        # return ans
-
-        # n=len(nums) # This uses prefix/postfix but space complexity not great
-        # ans, pre, suf = [0] * n, [0] * n, [0] * n
-        # pre[0] = 1
-        # suf[n-1] = 1
-
-        # for i in range(1, n):
-        #     pre[i] = pre[i-1]*nums[i-1]
-
-        # for i in range(n-2, -1, -1):
-        #     suf[i] = suf[i+1]*nums[i+1]
-
-        # for i in range(n):
-        #     ans[i] = pre[i]*suf[i]
-
-        # return ans
-        
-        ans = [1]*len(nums)
+        res = [1] * len(nums)
 
         prefix = 1
         for i in range(len(nums)):
-            ans[i] = prefix
+            res[i] = prefix
             prefix *= nums[i]
-
+        
         postfix = 1
-        for i in range(len(nums)-1, -1, -1):
-            ans[i] *= postfix
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
             postfix *= nums[i]
 
-        return ans
+        return res
