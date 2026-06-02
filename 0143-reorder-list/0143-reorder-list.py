e5160dc25752d3d00a03b9a83dacf4837e8b48d9
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-06-02 17:29:59 -0400

    Time: 3 ms (52.54%), Space: 27.9 MB (21.85%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -15,18 +15,20 @@ class Solution:
             fast = fast.next.next
 
         # Step 2: Reverse the second half
-        second = slow.next
+        temp = slow.next
         prev = slow.next = None
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
+        while temp:
+            node = temp.next
+            temp.next = prev
+            prev = temp
+            temp = node
         
         # Step 3: Alternatively add both of them one by one
         first, second = head, prev
         while second:
-            temp1, temp2 = first.next, second.next
+            temp = first.next
+            node = second.next
             first.next = second
-            second.next = temp1
-            first, second = temp1, temp2
+            second.next = temp
+            first = temp
+            second = node
