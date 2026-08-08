889e83ad42a5049a7e39a3b20e5bb91a563f8354
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2026-08-08 16:05:02 -0400

    Time: 100 ms (47.87%), Space: 30.8 MB (90.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,10 +1,34 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = [-i for i in nums]
-        heapq.heapify(heap)
-
-        while k > 1:
-            heapq.heappop(heap)
-            k -= 1
         
-        return -heap[0]
+        # Heap solution
+        heap=[]
+        for i in nums:
+            heapq.heappush(heap,i)
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return heap[0]
+
+        # Quickselect solution (due to it's better O(n) average case)
+
+        # k = len(nums) - k
+
+        # def quickSelect(l, r):
+        #     pivot_index = random.randint(l, r)
+        #     nums[pivot_index], nums[r] = nums[r], nums[pivot_index]
+        #     pivot, p = nums[r], l
+
+        #     for i in range(l, r):
+        #         if nums[i] <= pivot:
+        #             nums[p], nums[i] = nums[i], nums[p]
+        #             p += 1 
+        #     nums[p], nums[r] = nums[r], nums[p]
+            
+        #     if p > k:
+        #         return quickSelect(l, p - 1)
+        #     elif p < k:
+        #         return quickSelect(p + 1, r)
+        #     else:
+        #         return nums[p]
+        
+        # return quickSelect(0, len(nums) - 1)
